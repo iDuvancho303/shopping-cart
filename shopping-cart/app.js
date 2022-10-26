1755cd88a92c302c23cacd3eed20a158ba0bc6a4
Duvan <duvancho303@gmail.com>
2022-10-26 13:55:26 -0600

    adding code

--- shopping-cart/app.js
+++ shopping-cart/app.js
@@ -1,3 +1,4 @@
 // Variables
 const cart = document.querySelector('#cart');
+const shoppingCart = document.querySelector('#shopping-cart');
 const listOfProducts = document.querySelector('#products');
