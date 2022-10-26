f93126a07ee31d62a2a9504fd97a663309d408c9
Duvan <duvancho303@gmail.com>
2022-10-26 14:13:43 -0600

    adding code

--- shopping-cart/app.js
+++ shopping-cart/app.js
@@ -2,4 +2,10 @@
 const cart = document.querySelector('#cart');
 const shoppingCart = document.querySelector('#shopping-cart tbody');
 const listOfProducts = document.querySelector('#products');
-const emptyCart = document.querySelector('#empty-cart');
+const emptyCartBtn = document.querySelector('#empty-cart');
+
+
+// Event listeners 
+function loadEventListeners() {
+    listOfProducts.addEventListener('click', )
+}
